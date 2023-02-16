cc21c6933e532ea997468b73af1c21fa271ec6c0
John Yenter-Briars <jyenterbriars@root16.com>
2023-02-16 14:19:21 -0600

    plz work

--- CalendarSync.Console/Program.cs
+++ CalendarSync.Console/Program.cs
@@ -1,32 +1,68 @@
 ﻿using CalendarSync;
 using CalendarSync.Console;
 using CommandLine;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Win32.TaskScheduler;
 
-const uint DefaultDaysToSync = 7;
+const string DailyTaskName = "SyncCalendar";
+var configuration = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json")
+			.Build();
 
-Parser.Default.ParseArguments<Options>(args)
-				   .WithParsed<Options>(async o =>
-				   {
-					   var host = Host.CreateDefaultBuilder().Build();
-					   var logger = host.Services.GetRequiredService<ILogger<Program>>();
-					   logger.LogInformation("Logging established");
+var host = Host.CreateDefaultBuilder().Build();
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Logging established");
 
-					   var primaryAccountRefreshToken = o.PrimaryAccountRefreshToken;
-					   var secondaryAccountRefreshToken = o.SecondaryAccountRefreshToken;
-					   var clientId = o.ClientId;
-					   var orgConnectionString = o.OrgConnectionString;
+var primaryAccountRefreshToken = configuration["PrimaryAccountRefreshToken"];
+var primaryAccountSubjectPrefix = configuration["PrimaryAccountSubjectPrefix"];
+var secondaryAccountRefreshToken = configuration["PrimaryAccountRefreshToken"];
+var secondaryAccountSubjectPrefix = configuration["SecondaryAccountSubjectPrefix"];
+var clientId = configuration["PrimaryAccountRefreshToken"];
+var orgConnectionString = configuration["PrimaryAccountRefreshToken"];
+var daysToSync = uint.Parse(configuration["DaysToSync"]);
 
-					   var daysToSync = o.DaysToSync != 0 ? o.DaysToSync : DefaultDaysToSync;
+try
+{
+	using TaskService ts = new TaskService();
+	if (ts.GetTask(DailyTaskName) == null)
+	{
+		TaskDefinition td = ts.NewTask();
+		td.RegistrationInfo.Description = "Every day at 9 am sync calendars";
 
-					   var source = new SecondaryAccToPrimaryAccProfile(secondaryAccountRefreshToken, o.SecondaryAccountSubjectPrefix);
-					   var dest = new PrimaryAccToSecondaryAccProfile(primaryAccountRefreshToken, o.PrimaryAccountSubjectPrefix);
-					   var service = new CalendarSyncService(dest, source, logger, clientId, orgConnectionString);
-					   var startTime = DateTime.UtcNow;
-					   var endTime = startTime.AddDays(daysToSync);
+		DailyTrigger trigger = new()
+		{
+			StartBoundary = DateTime.Today + new TimeSpan(10, 45, 0),
+			DaysInterval = 1
+		};
+		td.Triggers.Add(trigger);
 
-					   await service.SyncRangeBidirectionalAsync(startTime.ToString("O"), endTime.ToString("O"));
-				   });
+		var dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/CalendarSync";
 
+		td.Actions.Add(new ExecAction($"{dir}/CalendarSync.Console.exe", dir, null));
+
+		ts.RootFolder.RegisterTaskDefinition(DailyTaskName, td);
+	}
+
+	var source = new SecondaryAccToPrimaryAccProfile(secondaryAccountRefreshToken, secondaryAccountSubjectPrefix);
+	var dest = new PrimaryAccToSecondaryAccProfile(primaryAccountRefreshToken, primaryAccountSubjectPrefix);
+	var service = new CalendarSyncService(dest, source, logger, clientId, orgConnectionString);
+	var startTime = DateTime.UtcNow;
+	var endTime = startTime.AddDays(daysToSync);
+
+	Console.WriteLine("TESTTTTTT");
+	Console.ReadKey();
+
+	//await service.SyncRangeBidirectionalAsync(startTime.ToString("O"), endTime.ToString("O"));
+
+}
+catch (Exception e)
+{
+	Console.WriteLine(e.ToString());
+}
+finally
+{
+	Console.ReadKey();
+}
