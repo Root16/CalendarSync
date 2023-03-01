469be81401fcb4fbab020fa2aba5bc9f2055cece
John Yenter-Briars <jyenterbriars@root16.com>
2023-03-01 15:05:48 -0600

    format

--- Calendula.Console/Program.cs
+++ Calendula.Console/Program.cs
@@ -1,21 +1,24 @@
 ﻿using Calendula;
-using Calendula.Console;
-using CommandLine;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Win32.TaskScheduler;
-const string DailyTaskName = "SyncCalendar";
+
+const string DailyTaskName = "Calendula";
 var configuration = new ConfigurationBuilder()
  .AddJsonFile("appsettings.json")
  .Build(); var host = Host.CreateDefaultBuilder().Build();
+
 var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
 logger.LogInformation(@"
             BE NOT AFRAID
             Your calendars will begin to sync
             DO NOT close this window until you see the final logging message.
-"); var primaryAccountRefreshToken = configuration["PrimaryAccountRefreshToken"];
+");
+
+var primaryAccountRefreshToken = configuration["PrimaryAccountRefreshToken"];
 var primaryAccountSubjectPrefix = configuration["PrimaryAccountSubjectPrefix"];
 var secondaryAccountRefreshToken = configuration["SecondaryAccountRefreshToken"];
 var secondaryAccountSubjectPrefix = configuration["SecondaryAccountSubjectPrefix"];
@@ -23,24 +26,40 @@ var hour24Time = int.Parse(configuration["Hour24Time"]);
 var minute24Time = int.Parse(configuration["Minute24Time"]);
 var clientId = configuration["ClientId"];
 var orgConnectionString = configuration["OrgConnectionString"];
-var daysToSync = uint.Parse(configuration["DaysToSync"]); try
+var daysToSync = uint.Parse(configuration["DaysToSync"]);
+
+try
 {
     using TaskService ts = new TaskService();
-    var CalendulaTask = ts.GetTask(DailyTaskName); if (CalendulaTask != null)
+    var CalendulaTask = ts.GetTask(DailyTaskName);
+
+    if (CalendulaTask != null)
     {
         ts.RootFolder.DeleteTask(DailyTaskName);
     }
+
     TaskDefinition td = ts.NewTask();
+
     td.RegistrationInfo.Description = "Every day at 9 am sync calendars"; DailyTrigger trigger = new()
     {
         StartBoundary = DateTime.Today + new TimeSpan(hour24Time, minute24Time, 0),
         DaysInterval = 1
     };
-    td.Triggers.Add(trigger); var dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/Calendula"; td.Actions.Add(new ExecAction($"{dir}/Calendula.Console.exe", dir, null)); ts.RootFolder.RegisterTaskDefinition(DailyTaskName, td); var source = new SecondaryAccToPrimaryAccProfile(secondaryAccountRefreshToken, secondaryAccountSubjectPrefix);
+
+    var dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/Calendula";
+
+    td.Triggers.Add(trigger);
+    td.Actions.Add(new ExecAction($"{dir}/Calendula.Console.exe", dir, null));
+    ts.RootFolder.RegisterTaskDefinition(DailyTaskName, td);
+
+    var source = new SecondaryAccToPrimaryAccProfile(secondaryAccountRefreshToken, secondaryAccountSubjectPrefix);
     var dest = new PrimaryAccToSecondaryAccProfile(primaryAccountRefreshToken, primaryAccountSubjectPrefix);
     var service = new CalendulaService(dest, source, logger, clientId, orgConnectionString);
     var startTime = DateTime.UtcNow;
-    var endTime = startTime.AddDays(daysToSync); await service.SyncRangeBidirectionalAsync(startTime.ToString("O"), endTime.ToString("O")); logger.LogInformation(@"
+    var endTime = startTime.AddDays(daysToSync);
+    await service.SyncRangeBidirectionalAsync(startTime.ToString("O"), endTime.ToString("O"));
+
+    logger.LogInformation(@"
             Calendar syncing complete! 
             Go forth about your day and be productive.
     ");
