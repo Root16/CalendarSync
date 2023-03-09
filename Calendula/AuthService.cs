c1878e2e5c5b88ce0064acfd857546be3e4a91ce
Paul Lohmuller <plohm12@gmail.com>
2023-03-09 16:10:09 -0600

    Formatting

--- Calendula/AuthService.cs
+++ Calendula/AuthService.cs
@@ -6,10 +6,10 @@ namespace Calendula
 {
     public class AuthService
     {
-		private const string TokenCacheFileName = "CalendulaTokenCache";
+        private const string TokenCacheFileName = "CalendulaTokenCache";
 
-		private IPublicClientApplication ClientApp;
-		private MsalCacheHelper Cache;
+        private IPublicClientApplication ClientApp;
+        private MsalCacheHelper Cache;
 
         protected AuthService(IPublicClientApplication clientApp, MsalCacheHelper cache)
         {
@@ -19,46 +19,46 @@ namespace Calendula
 
         public static async Task<AuthService> BuildAsync(string clientId)
         {
-			var storageProperties = new StorageCreationPropertiesBuilder(TokenCacheFileName, MsalCacheHelper.UserRootDirectory)
-				.Build();
+            var storageProperties = new StorageCreationPropertiesBuilder(TokenCacheFileName, MsalCacheHelper.UserRootDirectory)
+                .Build();
 
-			var cache = await MsalCacheHelper.CreateAsync(storageProperties);
+            var cache = await MsalCacheHelper.CreateAsync(storageProperties);
 
-			var app = PublicClientApplicationBuilder.Create(clientId)
-				.WithRedirectUri("http://localhost")
-				.Build();
+            var app = PublicClientApplicationBuilder.Create(clientId)
+                .WithRedirectUri("http://localhost")
+                .Build();
 
-			cache.RegisterCache(app.UserTokenCache);
+            cache.RegisterCache(app.UserTokenCache);
 
-			return new AuthService(app, cache);
-		}
+            return new AuthService(app, cache);
+        }
 
         public async Task<string> GetToken(string username)
         {
-			var accounts = await ClientApp.GetAccountsAsync();
-			var account = accounts.FirstOrDefault(a => a.Username == username);
+            var accounts = await ClientApp.GetAccountsAsync();
+            var account = accounts.FirstOrDefault(a => a.Username == username);
 
-			var scopes = new[] { "offline_access", "https://graph.microsoft.com/Calendars.ReadWrite" };
-			AuthenticationResult authResult;
+            var scopes = new[] { "offline_access", "https://graph.microsoft.com/Calendars.ReadWrite" };
+            AuthenticationResult authResult;
 
-			try
-			{
-				authResult = await ClientApp.AcquireTokenSilent(scopes, account)
-					.ExecuteAsync();
-			}
-			catch (MsalUiRequiredException)
-			{
-				var tokenBuilder = ClientApp.AcquireTokenInteractive(scopes)
-					.WithPrompt(Prompt.NoPrompt);
+            try
+            {
+                authResult = await ClientApp.AcquireTokenSilent(scopes, account)
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException)
+            {
+                var tokenBuilder = ClientApp.AcquireTokenInteractive(scopes)
+                    .WithPrompt(Prompt.NoPrompt);
 
-				tokenBuilder = account == null
-					? tokenBuilder.WithLoginHint(username)
-					: tokenBuilder.WithAccount(account);
+                tokenBuilder = account == null
+                    ? tokenBuilder.WithLoginHint(username)
+                    : tokenBuilder.WithAccount(account);
 
-				authResult = await tokenBuilder.ExecuteAsync();
-			}
+                authResult = await tokenBuilder.ExecuteAsync();
+            }
 
-			return authResult.AccessToken;
-		}
+            return authResult.AccessToken;
+        }
     }
 }
